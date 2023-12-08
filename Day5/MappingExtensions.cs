258ed0714a1a8b0e88514b92a6620a958a92dded
Roy Morten Nordbø <36782746+RoyMN@users.noreply.github.com>
2023-12-08 06:32:53 +0100

    Fixed bug on edge-case with graph

--- Day5/MappingExtensions.cs
+++ Day5/MappingExtensions.cs
@@ -2,5 +2,57 @@ namespace Extensions;
 
 public static class MappingExtensions
 {
+    public static void Deconstruct(this long[] value, out long v1, out long v2, out long v3)
+    {
+        if (value.Length < 3)
+            throw new ArgumentException("Array must have at least 3 elements", nameof(value));
+        v1 = value[0];
+        v2 = value[1];
+        v3 = value[2];
+    }
 
+    public static List<RangeNode> ParseSeeds(this string[] input)
+    {
+        // Extract and parse seed numbers
+        var seedLine = input[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return seedLine.Skip(1).Select(long.Parse).ToList().Select(s => new RangeNode(s, s + 1)).ToList();
+    }
+
+    public static List<RangeNode> ParseSeedRanges(this string[] input)
+    {
+        var seedLine = input[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var seedRanges = new List<RangeNode>();
+        for (int i = 1; i < seedLine.Length; i += 2)
+        {
+            long start = long.Parse(seedLine[i]);
+            long length = long.Parse(seedLine[i + 1]);
+            seedRanges.Add(new RangeNode(start, start+length));
+        }
+        return seedRanges;
+    }
+
+    public static Dictionary<string, List<MappingEdge>> ParseMappings(this string[] input)
+    {
+        var mappings = new Dictionary<string, List<MappingEdge>>();
+        string currentCategory = "";
+
+        foreach (var line in input.Skip(1))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+
+            if (line.Contains("map:"))
+            {
+                currentCategory = line.Split(':')[0];
+                mappings[currentCategory] = [];
+            }
+            else
+            {
+                (long destStart, long sourceStart, long length) = line.Split(' ').Select(long.Parse).ToArray();
+
+                mappings[currentCategory].Add(new MappingEdge(sourceStart, destStart, length));
+            }
+        }
+        return mappings;
+    }
 }
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,313 +1,134 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
+using Extensions;
 
 var example = false;
 
-//Part1();
+Part1();
 Part2();
 
 void Part1()
 {
+    // Converted to be align with Part2
+    var sw = Stopwatch.StartNew();
     var input = example ? File.ReadAllLines("./Day5.example-1") : File.ReadAllLines("./Day5.input-1");
-    var res = SolvePart1(input);
-    Console.WriteLine(res);
+    var seedRanges = input.ParseSeeds();
+    var mappings = input.ParseMappings();
+    List<RangeNode> finalRanges = TraverseGraph(seedRanges, mappings);
+    var lowestLocation = finalRanges.Min(r => r.Start);
+    sw.Stop();
+    Console.WriteLine($"Lowest Location found: {lowestLocation} in {sw.ElapsedMilliseconds}ms");
 }
 
 void Part2()
 {
-    var input = example ? File.ReadAllLines("./Day5.example-2") : File.ReadAllLines("./Day5.input-2");
     var sw = Stopwatch.StartNew();
-    var res = SolvePart2(input);
+    var input = example ? File.ReadAllLines("./Day5.example-1") : File.ReadAllLines("./Day5.input-1");
+    var seedRanges = input.ParseSeedRanges();
+    var mappings = input.ParseMappings();
+    List<RangeNode> finalRanges = TraverseGraph(seedRanges, mappings);
+    var lowestLocation = finalRanges.Min(r => r.Start);
     sw.Stop();
-    Console.WriteLine($"{res} found in {sw.ElapsedMilliseconds}ms");
+    Console.WriteLine($"Lowest Location found: {lowestLocation} in {sw.ElapsedMilliseconds}ms");
 }
 
-int SolvePart1(string[] input)
+static List<RangeNode> TraverseGraph(List<RangeNode> seedRanges, Dictionary<string, List<MappingEdge>> mappings)
 {
-    var seeds = ParseSeeds(input);
-    var mappings = ParseMappings(input[1..]);
-    var lowestLocation = GetLowestLocation(seeds, mappings);
-    return lowestLocation;
-}
+    var currentRanges = seedRanges;
 
-long SolvePart2(string[] input)
-{
-    var seeds = ParseSeedRanges(input);
-    var mappings = ParseMappingsMulti(input[1..]); // Skipping the first line (seeds line)
-    var lowestLocation = GetLowestLocationMulti(seeds, mappings);
-
-    // Only for validation. Takes a long time to run, bruteforce checking all numbers up to lowestLocation (naive checking, assumes traversing is done correctly)
-    // bool isValid = TestMappings(mappings, seeds, lowestLocation);
-    // var msg = isValid ? $"Mapping to {lowestLocation} is indeed the lowest possible mapping." : "Mapping to a lower number is possible.";
-    // Console.WriteLine(msg);
-    return lowestLocation;
-}
-
-static List<int> ParseSeeds(string[] input)
-{
-    // Extract and parse seed numbers
-    var seedLine = input[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    return seedLine.Skip(1).Select(int.Parse).ToList();
-}
-
-static List<Range> ParseSeedRanges(string[] input)
-{
-    var seedLine = input[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    var ranges = new List<Range>();
-    for (int i = 1; i < seedLine.Length; i += 2)
+    foreach (var category in mappings.Keys)
     {
-        // Read start and length of each seed range
-        ranges.Add(new Range(long.Parse(seedLine[i]),long.Parse(seedLine[i + 1])));
+        var sortedMappings = mappings[category].OrderBy(e => e.SourceStart).ToList();
+
+        var nextRanges = new List<RangeNode>();
+        foreach (RangeNode range in currentRanges)
+        {
+            var ranges = TransformRange(range, sortedMappings);
+            nextRanges.AddRange(ranges);
+        }
+        currentRanges = nextRanges;
     }
-    return ranges;
+    return currentRanges;
 }
 
-static Dictionary<string, List<RangeMapping>> ParseMappingsMulti(string[] input)
+static List<RangeNode> TransformRange(RangeNode range, List<MappingEdge> mappingEdges)
 {
-    var mappings = new Dictionary<string, List<RangeMapping>>();
-    string currentMap = "";
-    foreach (var line in input)
-    {
-        if (string.IsNullOrWhiteSpace(line))
-            continue;
-
-        if (line.Contains("map:"))
-        {
-            currentMap = line.Split(':')[0];
-            mappings[currentMap] = new List<RangeMapping>();
-        }
-        else
-        {
-            var parts = line.Split(' ').Select(long.Parse).ToArray();
-            mappings[currentMap].Add(new RangeMapping(parts[1], parts[0], parts[2]));
-        }
-    }
-
-    return mappings;
-}
-
-static Dictionary<string, Dictionary<int, int>> ParseMappings(string[] input)
-{
-    var mappings = new Dictionary<string, Dictionary<int, int>>();
-    string currentMap = "";
-    foreach (var line in input)
-    {
-        if (string.IsNullOrWhiteSpace(line))
-            continue;
-
-        if (line.Contains("map:"))
-        {
-            currentMap = line.Split(':')[0];
-            mappings[currentMap] = [];
-        }
-        else
-        {
-            var parts = line.Split(' ').Select(int.Parse).ToArray();
-            int destStart = parts[0];
-            int srcStart = parts[1];
-            int range = parts[2];
-
-            for (int i = 0; i < range; i++)
-            {
-                mappings[currentMap][srcStart + i] = destStart + i;
-            }
-        }
-    }
-
-    return mappings;
-}
-
-static int GetLowestLocation(List<int> seeds, Dictionary<string, Dictionary<int, int>> mappings)
-{
-    int lowestLocation = int.MaxValue;
-    foreach (var seed in seeds)
-    {
-        int current = seed;
-        foreach (var map in mappings)
-        {
-            current = map.Value.ContainsKey(current) ? map.Value[current] : current;
-        }
-        lowestLocation = Math.Min(lowestLocation, current);
-    }
-    return lowestLocation;
-}
-
-/**
-* Gets the lowest location that maps to a seed number.
-* This is done by transforming each seed number through the mappings, and then finding the lowest location among the transformed numbers.
-* It's done inner-out, so that the lowest location is found first.
-*/
-static long GetLowestLocationMulti(List<Range> seedRanges, Dictionary<string, List<RangeMapping>> mappings)
-{
-    var allTransformedRanges = new List<Range>();
-
-    foreach (var seedRange in seedRanges)
-    {
-        var transformedRange = new List<Range> { seedRange };
-
-        foreach (var map in mappings)
-        {
-            var nextTransformedRange = new List<Range>();
-
-            foreach (var range in transformedRange)
-            {
-                nextTransformedRange.AddRange(TransformRange(range, map.Value));
-            }
-
-            transformedRange = MergeRanges(nextTransformedRange);
-        }
-
-        allTransformedRanges.AddRange(transformedRange);
-    }
-
-    // Find the lowest starting value among all ranges
-    return allTransformedRanges.Min(r => r.Start);
-}
-
-/**
-* Transforms a range to its list of transformed ranges, based on a list of mappings.
-*/
-static List<Range> TransformRange(Range range, List<RangeMapping> mappings)
-{
-    var transformedRanges = new List<Range>();
+    var transformedRanges = new List<RangeNode>();
     long currentStart = range.Start;
 
-    foreach (var mapping in mappings)
+    for (int i = 0; i < mappingEdges.Count; i++)
     {
-        long mappingEnd = mapping.SourceStart + mapping.Length;
-        long rangeEnd = range.Start + range.Length;
-
-        // Check for overlap
-        if (currentStart < mappingEnd && rangeEnd > mapping.SourceStart)
+        if (currentStart == range.End)
         {
-            // Handle the part before the overlap
-            if (currentStart < mapping.SourceStart)
-            {
-                transformedRanges.Add(new Range(currentStart, mapping.SourceStart - currentStart));
-                currentStart = mapping.SourceStart;
-            }
-
-            // Handle the overlapping part
-            long overlapEnd = Math.Min(mappingEnd, rangeEnd);
-            long offset = currentStart - mapping.SourceStart;
-            transformedRanges.Add(new Range(mapping.DestStart + offset, overlapEnd - currentStart));
-            currentStart = overlapEnd;
+            // Nothing more to map
+            break;
         }
-
-        // Break if the rest of the range is beyond this mapping
-        if (currentStart >= rangeEnd) break;
+        long edgeStart = mappingEdges[i].SourceStart;
+        long edgeEnd = mappingEdges[i].SourceEnd;
+        long edgeDiff = mappingEdges[i].Diff;
+        if (currentStart > edgeEnd)
+        {
+            // Nothing more to map here (but maybe later)
+            continue;
+        }
+        if (currentStart < edgeStart)
+        {
+            // Map the non-overlapping segment to itself
+            transformedRanges.Add(new RangeNode(currentStart, edgeStart));
+            currentStart = edgeStart;
+        }
+        if (currentStart < edgeEnd)
+        {
+            // Map the overlapping segment
+            var segmentEnd = Math.Min(range.End, edgeEnd);
+            if (segmentEnd > currentStart)
+            {
+                transformedRanges.Add(new RangeNode(currentStart+edgeDiff, segmentEnd+edgeDiff));
+            }
+            currentStart = segmentEnd;
+        }
     }
-
-    // Handle any remaining part of the range after the last mapping
-    if (currentStart < range.Start + range.Length)
+    // If any part of the range remains after all mappings, map it to itself
+    if (currentStart < range.End)
     {
-        transformedRanges.Add(new Range(currentStart, range.Start + range.Length - currentStart));
+        transformedRanges.Add(new RangeNode(currentStart, range.End));
     }
 
     return transformedRanges;
 }
 
-/**
- * Merges a list of ranges into a list of merged ranges.
- * Requires the input ranges to be sorted by start value.
- * Example: [0, 5], [5, 5], [10, 5] -> [0, 10], [10, 5]
- *
- * Should not exist in the dataset, so probably not needed here.
- *
- */
-static List<Range> MergeRanges(List<Range> ranges)
-{
-    if (ranges.Count <= 1) return ranges;
-
-    // Sort ranges by start value
-    ranges.Sort((a, b) => a.Start.CompareTo(b.Start));
-
-    var mergedRanges = new List<Range>();
-    var currentRange = ranges[0];
-
-    foreach (var range in ranges.Skip(1))
-    {
-        if (currentRange.Start + currentRange.Length >= range.Start)
-        {
-            // Extend the current range if it overlaps or is contiguous with the next range
-            currentRange = new Range(currentRange.Start, Math.Max(currentRange.Start + currentRange.Length, range.Start + range.Length) - currentRange.Start);
-        }
-        else
-        {
-            mergedRanges.Add(currentRange);
-            currentRange = range;
-        }
-    }
-
-    // Add the last range
-    mergedRanges.Add(currentRange);
-
-    return mergedRanges;
-}
-
-#region Validation code
-static bool TestMappings(Dictionary<string, List<RangeMapping>> mappings, List<Range> seedRanges, long testUpTo)
-{
-    var reverseMappings = CreateReverseMappings(mappings);
-
-    for (long location = 0; location <= testUpTo; location++)
-    {
-        long seed = ReverseTransform(location, reverseMappings);
-        if (IsWithinSeedRanges(seed, seedRanges))
-        {
-            return false; // Found a lower location that maps back to a valid seed range
-        }
-    }
-
-    return true; // No lower location maps back to a valid seed range
-}
-
-static Dictionary<string, List<RangeMapping>> CreateReverseMappings(Dictionary<string, List<RangeMapping>> mappings)
-{
-    var reverseMappings = new Dictionary<string, List<RangeMapping>>();
-    foreach (var map in mappings)
-    {
-        reverseMappings[map.Key] = new List<RangeMapping>();
-        foreach (var rangeMapping in map.Value)
-        {
-            reverseMappings[map.Key].Add(new RangeMapping(rangeMapping.DestStart, rangeMapping.SourceStart, rangeMapping.Length));
-        }
-    }
-    return reverseMappings;
-}
-
-static long ReverseTransform(long number, Dictionary<string, List<RangeMapping>> reverseMappings)
-{
-    foreach (var map in reverseMappings)
-    {
-        foreach (var mapping in map.Value)
-        {
-            long mappingEnd = mapping.SourceStart + mapping.Length;
-            if (number >= mapping.SourceStart && number < mappingEnd)
-            {
-                long offset = number - mapping.SourceStart;
-                number = mapping.DestStart + offset;
-                break; // Break the inner loop once a mapping is applied
-            }
-        }
-    }
-    return number;
-}
-
-static bool IsWithinSeedRanges(long number, List<Range> seedRanges)
-{
-    foreach (var range in seedRanges)
-    {
-        if (number >= range.Start && number < range.Start + range.Length)
-        {
-            return true; // Number is within a seed range
-        }
-    }
-    return false;
-}
-
-#endregion
-
 #region Models
-record Range(long Start, long Length);
-record RangeMapping(long SourceStart, long DestStart, long Length);
+
+public class RangeNode
+{
+    public long Start { get; set; }
+    public long End { get; set; }
+
+    public RangeNode(long start, long end)
+    {
+        Start = start;
+        End = end;
+    }
+    public override string ToString()
+    {
+        return $"Start: {Start}, End: {End}";
+    }
+}
+
+public class MappingEdge(long sourceStart, long destinationStart, long length) : IComparable<MappingEdge>
+{
+    public long SourceStart { get; set; } = sourceStart;
+    public long DestinationStart { get; set; } = destinationStart;
+    public long Length { get; set; } = length;
+    public long SourceEnd => SourceStart + Length;
+    public long Diff => DestinationStart - SourceStart;
+    public int CompareTo(MappingEdge? other)
+    {
+        if (other == null) return 1;
+        return SourceStart.CompareTo(other.SourceStart);
+    }
+    public override string ToString()
+    {
+        return $"SourceStart: {SourceStart}, DestinationStart: {DestinationStart}, Length: {Length}";
+    }
+}
 #endregion
