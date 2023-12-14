b3cb13540d3c3799e9c543c13d2f68079d25987b
Roy Morten Nordbø <36782746+RoyMN@users.noreply.github.com>
2023-12-14 03:26:21 +0100

    Byebye RegEx

---
+++ Day3/MappingExtensions.cs
@@ -0,0 +1,148 @@
+using System.Diagnostics.CodeAnalysis;
+using Point = (int x, int y);
+using PointedChar = (char ch, (int x, int y) p);
+using PointedString = (string s, (int x, int y) p);
+
+namespace Extensions;
+
+public static class MappingExtensions
+{
+    /// <summary>
+    /// Returns the char at <c>colIdx</c> in <c>line</c>.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <param name="colIdx"></param>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    public static bool TryGet([NotNullWhen(true)] this string line, int colIdx, out char ch)
+    {
+        ch = char.MinValue;
+        if (colIdx < 0 || colIdx > line.Length - 1) return false;
+        ch = line[colIdx];
+        return true;
+    }
+
+    /// <summary>
+    /// Returns the circumference of <c>p</c>.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static IEnumerable<Point> Circumference(this Point p)
+    {
+        for (int i = p.y - 1; i < p.y + 2; i++)
+        {
+            for (int j = p.x - 1; j < p.x + 2; j++)
+            {
+                if (j == p.x && i == p.y) continue;
+                yield return (j, i);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the char at <c>p</c> in <c>lines</c>.
+    /// </summary>
+    /// <param name="lines"></param>
+    /// <param name="p"></param>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    public static bool TryGet([NotNullWhen(true)] this string[] lines, Point p, out char ch)
+    {
+        ch = char.MinValue;
+        if (p.y < 0 || p.y > lines.Length - 1) return false;
+        if (!lines[p.y].TryGet(p.x, out ch)) return false;
+        return true;
+    }
+
+    /// <summary>
+    /// Enumerates all adjacent chars to <c>p</c> in <c>lines</c>.
+    /// </summary>
+    /// <param name="lines"></param>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static IEnumerable<PointedChar> AdjacentChars(this string[] lines, Point p)
+    {
+        foreach(var point in p.Circumference())
+        {
+            if (lines.TryGet(point, out char ch))
+            {
+                yield return (ch, point);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the string of a number spanning on <c>line</c> from <c>p</c>.
+    /// </summary>
+    /// <param name="line">A line containing the number.</param>
+    /// <param name="p">A point on the line and the number.</param>
+    /// <returns><see cref="(string number, (int x, int y))"/></returns>
+    public static PointedString StringNumber(this string line, Point p)
+    {
+        if (p.x < 0 || p.x > line.Length - 1) throw new();
+        if (!char.IsDigit(line[p.x])) throw new();
+        var startIdx = p.x;
+        while (line.TryGet(startIdx - 1, out char ch))
+        {
+            if (char.IsDigit(ch))
+            {
+                startIdx -= 1;
+            }
+            else break;
+        }
+        var endIdx = startIdx;
+        var stringNumber = "";
+        while (line.TryGet(endIdx, out char ch))
+        {
+            if (char.IsDigit(ch))
+            {
+                stringNumber += ch;
+                endIdx += 1;
+            }
+            else break;
+        }
+        return (stringNumber, (startIdx, p.y));
+    }
+    
+    /// <summary>
+    /// Checks if the <c>PointedString</c> contains the given <c>Point</c>.
+    /// </summary>
+    /// <returns>boolean</returns>
+    public static bool Contains(this PointedString str, Point p)
+    {
+        if (str.p.y != p.y) return false;
+        else return str.p.x <= p.x && p.x <= str.p.x + str.s.Length;
+    }
+
+    /// <summary>
+    /// Finds all the <c>symbol</c>'s in <c>lines</c>.
+    /// </summary>
+    /// <param name="lines"></param>
+    /// <param name="symbol"></param>
+    /// <returns><c>Point</c> for each <c>symbol</c></returns>
+    public static IEnumerable<Point> FindSymbols(this string[] lines, char symbol)
+    {
+        if (lines.Length == 0) throw new();
+        var innerLength = lines[0].Length;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < innerLength; j++)
+            {
+                if (lines[i][j] == symbol)
+                {
+                    yield return (j, i);
+                }
+            }
+        }
+    }
+
+    public static void DisplayLines(this string[] lines)
+    {
+        Console.WriteLine();
+        foreach(var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+        Console.WriteLine();
+    }
+}
--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,259 +1,44 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using Extensions;
+using Point = (int x, int y);
+using PointedChar = (char ch, (int x, int y) p);
+using PointedString = (string s, (int x, int y) p);
 
-class Program
+var sw = Stopwatch.StartNew();
+var lines = File.ReadAllLines("./input1");
+var sum = 0;
+
+//lines.DisplayLines();
+
+var stars = lines.FindSymbols('*');
+
+foreach(var star in stars)
 {
-    static void Main(string[] args)
+    //Console.WriteLine($"Star at: {star}");
+    var adjacentChars = lines.AdjacentChars(star).Where(ch => char.IsDigit(ch.ch));
+    var numbers = new List<PointedString>();
+
+    foreach(var ch in adjacentChars)
     {
-        //Part1();
-        Part2();
+        if (!numbers.Any(n => n.Contains(ch.p)))
+        {
+            var stringNumber = lines[ch.p.y].StringNumber(ch.p);
+            numbers.Add(stringNumber);
+        }
     }
 
-    static void Part1()
+
+    if (numbers.Count == 2)
     {
-        string[] engineSchematic = File.ReadAllLines("./example.txt");
-
-        int missingPart = FindMissingPart(engineSchematic);
-        Console.WriteLine($"The missing part number is: {missingPart}");
+        //Console.WriteLine($"{sum} += {numbers[0].s} * {numbers[1].s}");
+        sum += int.Parse(numbers[0].s) * int.Parse(numbers[1].s);
     }
-
-    static void Part2()
+    else
     {
-        string[] engineSchematic = File.ReadAllLines("./input.txt");
-
-        int gear = FindGear(engineSchematic);
-        Console.WriteLine($"The gear number is: {gear}");
+        //Console.WriteLine($"Star has {numbers.Count} adjacent number(s), skipping...");
     }
-
-    static int FindGear(string[] engineSchematic)
-    {
-        int sum = 0;
-        string[] lines = engineSchematic;
-        for (int i = 0; i < lines.Length; i++)
-        {
-            string line = lines[i];
-
-            // A gear is any "*" that has exactly 2 adjacent numbers.
-            // Adjacent means either horizontally, vertically, or diagonally.
-            
-            // regex to find all * in a line
-            var matches = Regex.Matches(line, @"\*");
-
-            MatchCollection numbersSameLine = null;
-            MatchCollection numbersAboveLine = null;
-            MatchCollection numbersBelowLine = null;
-
-            numbersSameLine = Regex.Matches(line, @"\d+");
-            if (i > 0)
-            {
-                numbersAboveLine = Regex.Matches(lines[i - 1], @"\d+");
-            }
-            if (i < lines.Length - 1)
-            {
-                numbersBelowLine = Regex.Matches(lines[i + 1], @"\d+");
-            }
-
-            // loop through all * in the line
-            foreach(Match match in matches)
-            {
-                var adjacentMatches = new List<Match>();
-                foreach (Match number in numbersSameLine)
-                {
-                    if (MatchIsAdjacentTo(match.Index, number))
-                    {
-                        adjacentMatches.Add(number);
-                    }
-                }
-                if (numbersAboveLine != null)
-                {
-                    foreach (Match number in numbersAboveLine)
-                    {
-                        if (MatchIsAdjacentTo(match.Index, number))
-                        {
-                            adjacentMatches.Add(number);
-                        }
-                    }
-                }
-                if (numbersBelowLine != null)
-                {
-                    foreach (Match number in numbersBelowLine)
-                    {
-                        if (MatchIsAdjacentTo(match.Index, number))
-                        {
-                            adjacentMatches.Add(number);
-                        }
-                    }
-                }
-
-                if (adjacentMatches.Count == 2)
-                {
-                    sum += int.Parse(adjacentMatches[0].Value) * int.Parse(adjacentMatches[1].Value);
-                }
-            }
-        }
-        return sum;
-    }
-
-    static bool MatchIsAdjacentTo(int colIndex, Match match)
-    {
-        var matchColIndex = match.Index;
-        var matchColIndexEnd = matchColIndex + match.Length - 1;
-        for (int i = matchColIndex; i <= matchColIndexEnd; i++)
-        {
-            if (colIndex == i || colIndex == i - 1 || colIndex == i + 1)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static int FindMissingPart(string[] engineSchematic)
-    {
-        int sum = 0;
-        string[] lines = engineSchematic;
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var line = lines[i];
-            var matches = Regex.Matches(line, @"\d+");
-            foreach (Match match in matches)
-            {
-                var rowIndex = i;
-                var colIndex = match.Index;
-                var colIndexEnd = colIndex + match.Length - 1;
-                if (LineHasUpperSymbol(lines, rowIndex, colIndex, match.Length) || 
-                    LineHasLowerSymbol(lines, rowIndex, colIndex, match.Length) ||
-                    LineHasLeftSymbol(lines, rowIndex, colIndex) ||
-                    LineHasRightSymbol(lines, rowIndex, colIndexEnd) ||
-                    LineHasDiagonalUpperLeftSymbol(lines, rowIndex, colIndex) ||
-                    LineHasDiagonalUpperRightSymbol(lines, rowIndex, colIndexEnd) ||
-                    LineHasDiagonalLowerLeftSymbol(lines, rowIndex, colIndex) ||
-                    LineHasDiagonalLowerRightSymbol(lines, rowIndex, colIndexEnd))
-                {
-                    sum += int.Parse(match.Value);
-                }
-            }
-        }
-
-        return sum;
-    }
-
-    static bool LineHasUpperSymbol(string[] lines, int rowIndex, int colIndex, int length)
-    {
-        if (rowIndex == 0)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex - 1];
-        for (int i = 0; i < length; i++)
-        {
-            var symbol = line[colIndex + i];
-            if (symbol != '.' && !char.IsDigit(symbol))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool LineHasLowerSymbol(string[] lines, int rowIndex, int colIndex, int length)
-    {
-        if (rowIndex == lines.Length - 1)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex + 1];
-        for (int i = 0; i < length; i++)
-        {
-            var symbol = line[colIndex + i];
-            if (symbol != '.' && !char.IsDigit(symbol))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool LineHasLeftSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        var line = lines[rowIndex];
-        if (colIndex == 0)
-        {
-            return false;
-        }
-
-        var symbol = line[colIndex - 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
-
-    static bool LineHasRightSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        var line = lines[rowIndex];
-        if (colIndex == line.Length - 1)
-        {
-            return false;
-        }
-
-        var symbol = line[colIndex + 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
-
-    static bool LineHasDiagonalUpperLeftSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        if (rowIndex == 0 || colIndex == 0)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex - 1];
-        var symbol = line[colIndex - 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
-
-    static bool LineHasDiagonalUpperRightSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        if (rowIndex == 0)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex - 1];
-        if (colIndex == line.Length - 1)
-        {
-            return false;
-        }
-
-        var symbol = line[colIndex + 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
-
-    static bool LineHasDiagonalLowerLeftSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        if (rowIndex == lines.Length - 1 || colIndex == 0)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex + 1];
-        var symbol = line[colIndex - 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
-
-    static bool LineHasDiagonalLowerRightSymbol(string[] lines, int rowIndex, int colIndex)
-    {
-        if (rowIndex == lines.Length - 1)
-        {
-            return false;
-        }
-
-        var line = lines[rowIndex + 1];
-        if (colIndex == line.Length - 1)
-        {
-            return false;
-        }
-
-        var symbol = line[colIndex + 1];
-        return symbol != '.' && !char.IsDigit(symbol);
-    }
+    //Console.WriteLine();
 }
+sw.Stop();
+
+Console.WriteLine($"{Environment.NewLine}Total: {sum} found in {sw.ElapsedMilliseconds}ms.");
