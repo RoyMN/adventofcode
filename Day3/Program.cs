8bede3ffec9aab3afcca7f831d7b2321e3241a4f
Roy Morten Nordbø <36782746+RoyMN@users.noreply.github.com>
2023-12-14 03:35:33 +0100

    Added back part 1

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -4,41 +4,73 @@ using Point = (int x, int y);
 using PointedChar = (char ch, (int x, int y) p);
 using PointedString = (string s, (int x, int y) p);
 
-var sw = Stopwatch.StartNew();
-var lines = File.ReadAllLines("./input1");
-var sum = 0;
+Part1();
+Part2();
 
-//lines.DisplayLines();
-
-var stars = lines.FindSymbols('*');
-
-foreach(var star in stars)
+void Part1()
 {
-    //Console.WriteLine($"Star at: {star}");
-    var adjacentChars = lines.AdjacentChars(star).Where(ch => char.IsDigit(ch.ch));
-    var numbers = new List<PointedString>();
+    var sw = Stopwatch.StartNew();
+    var lines = File.ReadAllLines("./input1");
+    var sum = 0;
 
-    foreach(var ch in adjacentChars)
+    //lines.DisplayLines();
+
+    var symbols = lines.FindSymbols(null);
+
+    foreach(var symbol in symbols)
     {
-        if (!numbers.Any(n => n.Contains(ch.p)))
+        var adjacentChars = lines.AdjacentChars(symbol).Where(ch => char.IsDigit(ch.ch));
+        var numbers = new List<PointedString>();
+
+        foreach(var ch in adjacentChars)
         {
-            var stringNumber = lines[ch.p.y].StringNumber(ch.p);
-            numbers.Add(stringNumber);
+            if (!numbers.Any(n => n.Contains(ch.p)))
+            {
+                var stringNumber = lines[ch.p.y].StringNumber(ch.p);
+                numbers.Add(stringNumber);
+            }
+        }
+
+
+        foreach (var (s, _) in numbers)
+        {
+            sum += int.Parse(s);
         }
     }
+    sw.Stop();
 
-
-    if (numbers.Count == 2)
-    {
-        //Console.WriteLine($"{sum} += {numbers[0].s} * {numbers[1].s}");
-        sum += int.Parse(numbers[0].s) * int.Parse(numbers[1].s);
-    }
-    else
-    {
-        //Console.WriteLine($"Star has {numbers.Count} adjacent number(s), skipping...");
-    }
-    //Console.WriteLine();
+    Console.WriteLine($"{Environment.NewLine}Part 1: {sum} found in {sw.ElapsedMilliseconds}ms.");
 }
-sw.Stop();
 
-Console.WriteLine($"{Environment.NewLine}Total: {sum} found in {sw.ElapsedMilliseconds}ms.");
+void Part2()
+{
+    var sw = Stopwatch.StartNew();
+    var lines = File.ReadAllLines("./input1");
+    var sum = 0;
+
+    var stars = lines.FindSymbols('*');
+
+    foreach(var star in stars)
+    {
+        var adjacentChars = lines.AdjacentChars(star).Where(ch => char.IsDigit(ch.ch));
+        var numbers = new List<PointedString>();
+
+        foreach(var ch in adjacentChars)
+        {
+            if (!numbers.Any(n => n.Contains(ch.p)))
+            {
+                var stringNumber = lines[ch.p.y].StringNumber(ch.p);
+                numbers.Add(stringNumber);
+            }
+        }
+
+
+        if (numbers.Count == 2)
+        {
+            sum += int.Parse(numbers[0].s) * int.Parse(numbers[1].s);
+        }
+    }
+    sw.Stop();
+
+    Console.WriteLine($"{Environment.NewLine}Part 2: {sum} found in {sw.ElapsedMilliseconds}ms.");
+}
